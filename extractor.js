81cb27ddeec572772576b5216d105ca30be31744
Katie Atkinson <katilin.krouth@gmail.com>
2014-09-28 16:17:17 -0700

    This is the first commit of code for the extension. It lays down the groundwork for the basic functionality.

---
+++ extractor.js
@@ -0,0 +1,26 @@
+function quaryOnClick(info, tab) {
+  var extractor = findExtractorById(info.menuItemId)
+  var parsed = parseUrl(info.linkUrl)
+
+  for each (var query in parsed.queries.split("&") {
+    var query = query.split("=")
+    if (query[0] in extractor.queries) {
+      confirmCopy(query[1])
+    }
+  }
+
+  alert("No {} was found in the URL provided.".format(extractor.dataName)
+  return null
+}
+
+function confirmCopy(textToCopy) {
+  if (prompt("Press enter or CTR+C to copy", textToCopy) {
+    window.execcommand("copy")
+  }
+}
+
+var extractors = getExtractors();
+base_extractor_context = ["link"];
+
+for each (var extractor in extractors) {
+  chrome.contextMenus.create({"id": extractor[id], "title"="Copy "+extractor.dataName, "contexts"=base_extractor_context, "onclick"=quaryOnClick)}
---
+++ extractor_loader.js
@@ -0,0 +1,30 @@
+var DEFAULTS = {
+  extractors: {
+    {name: "YouTube Video ID",
+    dataName: "YouTube Video ID",
+    queries: ["v", "video_id"],
+    id: "default",
+    isActive: true,
+    }
+  },
+  isSet: false
+}
+
+function getOptions() {
+  return chrome.storage.sync.get(DEFAULTS)
+}
+
+function getExtractors() {
+  return getOptions().extractors
+}
+
+function findExtractorById(id) {
+  for each (var extractor in getExtractors()) {
+    if extractor.id == id {
+      return extractor
+    }
+  }
+
+  return nil
+}
+
